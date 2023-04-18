b822715bcb450d3c03ed0a987fb5934b65722000
aizeresalim <0424aizeka@gmail.com>
2023-04-18 21:56:56 +0600

    2

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -27,4 +27,27 @@ public class MyArrayList<T> implements MyList<T> {
         }
         elements[size++] = item;
     }
-
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(item)) {
+                System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                size--;
+                elements[size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
