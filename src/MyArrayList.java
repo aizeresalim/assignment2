a97d0957d62b94a0699a4be8a86a8471e1e4f394
aizeresalim <0424aizeka@gmail.com>
2023-04-18 21:47:33 +0600

    1

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -6,3 +6,25 @@ public class MyArrayList<T> implements MyList<T> {
         this.elements = new Object[10];
         this.size = 0;
     }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void add(T item) {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        elements[size++] = item;
+    }
+
