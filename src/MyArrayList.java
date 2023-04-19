243280388d94f31493f302445c9a7cf07ae4cbdf
aizeresalim <0424aizeka@gmail.com>
2023-04-19 17:47:09 +0600

    myarraylist and mylinkedlist

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,76 +1,11 @@
-public class MyArrayList<T> implements MyList<T> {
-    private Object[] elements;
+import java.util.Arrays;
+
+public class MyArrayList<T> implements MyList {
+    private T[] arr;
     private int size;
 
-    public MyArrayList() {
-        this.elements = new Object[10];
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
         this.size = 0;
     }
 
-    public int size() {
-        return this.size;
-    }
-
-    public boolean contains(Object o) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i].equals(o)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public void add(T item) {
-        if (size == elements.length) {
-            Object[] newElements = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newElements, 0, elements.length);
-            elements = newElements;
-        }
-        elements[size++] = item;
-    }
-    public void add(T item, int index) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException();
-        }
-        if (size == elements.length) {
-            Object[] newElements = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newElements, 0, elements.length);
-            elements = newElements;
-        }
-        System.arraycopy(elements, index, elements, index + 1, size - index);
-        elements[index] = item;
-        size++;
-    }
-    public boolean remove(T item) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i].equals(item)) {
-                System.arraycopy(elements, i + 1, elements, i, size - i - 1);
-                size--;
-                elements[size] = null;
-                return true;
-            }
-        }
-        return false;
-    }
-    public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        T item = (T) elements[index];
-        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
-        size--;
-        elements[size] = null;
-        return item;
-    }
-    public void clear() {
-        for (int i = 0; i < size; i++) {
-            elements[i] = null;
-        }
-        size = 0;
-    }
-
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        return (T) elements[index];
-    }
