aeaad00fac44aad0339c147214b9daca92f74819
aizeresalim <0424aizeka@gmail.com>
2023-04-19 20:46:43 +0600

    my array list commented

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,145 +1,166 @@
 public class MyArrayList<T> implements MyList {
     private T[] arr;
     private int size;
+
+    /**
+     * @function constructor
+     * @noparam
+     * @return void
+     **/
     MyArrayList(){
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
 
-    public void increaseBuffer(){
-        T[] newArr = (T[]) new Object[arr.length*2];
-        for(int i=0; i< arr.length; i++){
-            newArr[i]=arr[i];
-            public void increaseBufferIfNeeded(){
-                if(size == arr.length){
-                    T[] newArr = (T[]) new Object[arr.length*2];
-                    for(int i=0; i< arr.length; i++){
-                        newArr[i]=arr[i];
-                    }
-                    arr = newArr;
-                }
+    /**
+     * @function increaseBufferIfNeeded increase array buffer
+     * @noparams
+     * @return void
+     * **/
+    public void increaseBufferIfNeeded(){
+        if(size == arr.length){
+            T[] newArr = (T[]) new Object[arr.length*2];
+            @@ -16,22 +24,43 @@ public void increaseBufferIfNeeded(){
                 arr = newArr;
             }
-
-            public T getElement(int index) {
-                checkIndex(index);
-                return arr[index];
+        }
+        /**
+         * @function size returns the length of the array
+         * @noparams
+         * @return int
+         * **/
+        @Override
+        public int size() {
+            return this.size;
+        }
+        /**
+         * @function contains true if Object o is in array
+         * @param o
+         * @return int
+         * **/
+        @Override
+        public boolean contains(Object o) {
+            for(T element : arr) {
+                if (o.equals(element)) return true;
             }
-
-            public int getSize() {
-                return size;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-                return this.size;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                for(T element : arr) {
-                    if (o.equals(element)) return true;
-                }
-                return false;
-            }
-
-            @Override
-            public void add(Object item) {
-                if(size == arr.length){
-                    increaseBuffer();
-                }
-                increaseBufferIfNeeded();
-                arr[size++] = (T) item;
-            }
-            @Override
-            public void add(Object item, int index) {
-
-                checkIndex(index);
-                increaseBufferIfNeeded();
-                for(int i = size; i>index; i--) {
-                    arr[i] = arr[i-1];
-                }
+            return false;
+        }
+        /**
+         * @function add - adds an object to an array
+         * @param item object to add
+         * @return void
+         * **/
+        @Override
+        public void add(Object item) {
+            increaseBufferIfNeeded();
+            arr[size++] = (T) item;
+        }
+        /**
+         * @function add true if Object o in array
+         * @param item object to add
+         * @param index index where to add item
+         * @return void
+         * **/
+        @Override
+        public void add(Object item, int index) {
+            checkIndex(index);
+            @@ -42,7 +71,12 @@ public void add(Object item, int index) {
                 arr[index] = (T) item;
                 size++;
             }
-
             // 0(n^2)
+            /**
+             * @function remove - removes an object from an array
+             * @param item delete object
+             * @return boolean
+             * **/
+            // Complexity: 0(n^2)
             @Override
             public boolean remove(Object item) {
                 int index = indexOf(item);
-                if (index >= 0) {
-                    remove(index);
-                    return true;
+                @@ -52,7 +86,11 @@ public boolean remove(Object item) {
                 }
                 return false;
             }
 
+            /**
+             * @function remove - removes an object from an array
+             * @param index The index of the element to remove
+             * @return boolean
+             * **/
             @Override
             public Object remove(int index) {
-                return null;
                 checkIndex(index);
-                T temporary = this.arr[index];
-                for(int i= index + 1; i<size; i++){
-                    arr[i-1] = arr[i];
+                @@ -63,18 +101,30 @@ public Object remove(int index) {
+                    this.size--;
+                    return temporary;
                 }
-                this.size--;
-                return temporary;
-            }
 
-            public void clear(){
-                @@ -63,32 +71,39 @@ public void clear(){
+                /**
+                 * @function clear - clears the array
+                 * @noparam
+                 * @return void
+                 * **/
+                public void clear(){
+                    this.arr = (T[]) new Object[5];
+                    this.size = 0;
+                }
 
-                    @Override
-                    public Object get(int index) {
-                        return null;
-                        checkIndex(index);
-                        return arr[index];
-                    }
+                /**
+                 * @function get - returns an object from the array at its index
+                 * @param index
+                 * @return Object
+                 * **/
+                @Override
+                public Object get(int index) {
+                    checkIndex(index);
+                    return arr[index];
+                }
 
-                    @Override
-                    public int indexOf(Object o) {
-                        return 0;
-                        for (int i = 0; i < arr.length; i++) {
-                            if (arr[i].equals((T) o)) {
-                                return i;
-                            }
+                /**
+                 * @function indexOf - returns the index of the first occurrence of an object
+                 * @param o object to search
+                 * @return int
+                 * **/
+                @Override
+                public int indexOf(Object o) {
+                    for (int i = 0; i < arr.length; i++) {
+                        @@ -84,13 +134,22 @@ public int indexOf(Object o) {
                         }
                         return -1;
                     }
 
+                    /**
+                     * @function lastIndexOf returns the index of the last occurrence of an object in an array
+                     * @param o object to search
+                     * @return int
+                     * **/
                     @Override
                     public int lastIndexOf(Object o) {
-                        return 0;
                         int index = -1;
                         for (int i = 0; i < arr.length; i++) if (arr[i].equals((T) o) && i > index) index = i;
                         return index;
                     }
-
+                    /**
+                     * @function sort- sorts the array (bubble sort, O(n^2))
+                     * @noparam
+                     * @return void
+                     * **/
                     @Override
                     public void sort() {
-
-                    }
-
-                    public void delete(int index){
-                        checkIndex(index);
-                        for(int i= index + 1; i<size; i++){
-                            arr[i-1] = arr[i];
-                            for(int i = 0; i < size; i++) {
-                                for (int j = i; j < size; j++) {
-                                    if ((int) arr[j] < (int) arr[i]) {
-                                        T temp = arr[i];
-                                        arr[i] = arr[j];
-                                        arr[j] = temp;
-                                    }
-                                }
+                        for(int i = 0; i < size; i++) {
+                            @@ -103,6 +162,11 @@ public void sort() {
                             }
-                            size--;
                         }
-                        public void checkIndex(int index){
-                            private void checkIndex(int index){
-                                if(index < 0 || index>=size){
-                                    throw new IndexOutOfBoundsException();
-                                }
-
+                    }
+                    /**
+                     * @function checkIndex - checks the index for its validity within the data structure
+                     * @param index
+                     * @return void
+                     * **/
+                    private void checkIndex(int index){
+                        if(index < 0 || index>=size){
+                            throw new IndexOutOfBoundsException();
                         }
+                    }
+                }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -5,57 +5,55 @@ public class MyLinkedList<E> implements MyList{
         Node next;
         public Node(E v) {
             val = v;
+
+            previous = null;
+            next = null;
         }
     }
 
-    private Node<E> head;
+    @@ -31,7 +32,15 @@ public boolean contains(Object o) {
 
-    private Node<E> tail;
-    private int size;
+        @Override
+        public void add(Object item) {
 
-    MyLinkedList() {
-        size = 0;
-    }
+            Node<E> node = new Node<>((E) item);
+            if (size == 0) {
+                this.head = node;
+            } else {
+                node.previous = this.tail;
+                this.tail.next = node;
+            }
+            this.tail = node;
+            size++;
+        }
 
-    @Override
-    public int size() {
-        return this.size;
-    }
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-    @Override
-    public void add(Object item) {
-    }
-    @Override
-    public void add(Object item, int index) {
-    }
-    @Override
-    public boolean remove(Object item) {
-        return false;
-    }
-    @Override
-    public Object remove(int index) {
-        return null;
-    }
-    @Override
-    public void clear() {
-    }
-    @Override
-    public Object get(int index) {
-        return null;
-    }
-    @Override''
-    public int indexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public void sort() {
-    }
-}
+        @Override
+        public void add(Object item, int index) {
+        }
+        @Override
+        public boolean remove(Object item) {
+            return false;
+        }
+        @Override
+        public Object remove(int index) {
+            return null;
+        }
+        @Override
+        public void clear() {
+        }
+        @Override
+        public Object get(int index) {
+            return null;
+        }
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+        @Override
+        public void sort() {
+        }
     }
