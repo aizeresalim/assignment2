07876a96633f01cabe8429ee5fe4bbed1679e382
aizeresalim <0424aizeka@gmail.com>
2023-04-19 21:32:11 +0600

    findIndexedNode function

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -24,8 +24,8 @@ public class MyLinkedList<E> implements MyList{
         return false;
     }
     /**
-     * @function add
-     * @param item
+     * @function add добавляет объект в LinkedList
+     * @param item объект для добавления
      * @return void
      * **/
     @Override
@@ -50,56 +50,72 @@ public class MyLinkedList<E> implements MyList{
 
     @Override
     public Object remove(int index) {
-        return null;
         Node toSearch;
         while (index != 0) {
             toSearch = toSearch.next;
             index--;
+            checkIndex(index);
+            Node node = findIndexedNode(index);
+
+            if (node.previous == null) {
+                this.head = node.next;
+                this.head.previous = null;
+            }
+            else if (node.next == null) {
+                this.tail = node.previous;
+                this.tail.next = null;
+            }
+            else {
+                node.previous.next = node.next;
+                node.next.previous = node.previous;
+            }
+
+            this.size--;
+            return node.val;
+
+        }
+
+        @Override
+        public void clear() {
+        }
+        /**
+         * @function get возвращает объект из LinkedList под его индексом
+         * @param index индекс объекта
+         * @return E
+         * **/
+        @Override
+        public Object get(int index) {
+
+            return toSearch.val;
+            checkIndex(index);
+            return findIndexedNode(index).val;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+        @Override
+        public void sort() {
+
+        }
+        private Node findIndexedNode(int index) {
+            checkIndex(index);
+            Node toSearch;
+            toSearch = this.head;
+            while (index != 0) {
+                toSearch = toSearch.next;
+                index--;
+            }
+            return toSearch;
+        }
+        private void checkIndex(int index){
+            if(index < 0 || index>=size){
+                throw new IndexOutOfBoundsException();
+            }
         }
     }
-
-    @Override
-    public void clear() {
-    }
-    /**
-     * @function get
-     * @param index
-     * @return E
-     * **/
-    @Override
-    public Object get(int index) {
-        Node toSearch;
-        toSearch = this.head;
-        while (index != 0) {
-            toSearch = toSearch.next;
-            index--;
-        }
-        return toSearch.val;
-
-
-
-        return toSearch.val;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public void sort() {
-
-    }
-    private Node findIndexedNode(int index) {
-        Node toSearch;
-        toSearch = this.head;
-        while (index != 0) {
-            toSearch = toSearch.next;
-            index--;
-        }
-        return toSearch;
-    }
-}
