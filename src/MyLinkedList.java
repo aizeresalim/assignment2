22182887474574c447b37689e862e74ba3c244cd
aizeresalim <0424aizeka@gmail.com>
2023-04-19 20:50:33 +0600

    get function

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -5,55 +5,80 @@ public class MyLinkedList<E> implements MyList{
         Node next;
         public Node(E v) {
             val = v;
-
             previous = null;
             next = null;
         }
     }
-
-    @@ -31,7 +32,15 @@ public boolean contains(Object o) {
-
-        @Override
-        public void add(Object item) {
-
-            Node<E> node = new Node<>((E) item);
-            if (size == 0) {
-                this.head = node;
-            } else {
-                node.previous = this.tail;
-                this.tail.next = node;
-            }
-            this.tail = node;
-            size++;
-        }
-
-        @Override
-        public void add(Object item, int index) {
-        }
-        @Override
-        public boolean remove(Object item) {
-            return false;
-        }
-        @Override
-        public Object remove(int index) {
-            return null;
-        }
-        @Override
-        public void clear() {
-        }
-        @Override
-        public Object get(int index) {
-            return null;
-        }
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-        @Override
-        public void sort() {
-        }
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+    MyLinkedList() {
+        size = 0;
     }
+    @Override
+    public int size() {
+        return this.size;
+    }
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+    /**
+     * @function add добавляет объект в LinkedList
+     * @param item объект для добавления
+     * @return void
+     * **/
+    @Override
+    public void add(Object item) {
+        Node<E> node = new Node<>((E) item);
+        if (size == 0) {
+            this.head = node;
+        } else {
+            node.previous = this.tail;
+            this.tail.next = node;
+        }
+        this.tail = node;
+        size++;
+    }
+    @Override
+    public void add(Object item, int index) {
+    }
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+    @Override
+    public void clear() {
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+        Node toSearch;
+        toSearch = this.head;
+        while (index != 0) {
+            toSearch = toSearch.next;
+            index--;
+        }
+        return (E) toSearch.val;
+
+
+
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+    @Override
+    public void sort() {
+    }
+}
