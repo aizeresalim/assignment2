f00d2ceeb26f4316c904d6574e43ad12065c2e22
aizeresalim <0424aizeka@gmail.com>
2023-04-19 21:40:18 +0600

    clear func and add func comments

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -15,104 +15,86 @@ public class MyLinkedList<E> implements MyList{
     MyLinkedList() {
         size = 0;
     }
+
+    /**
+     * @function size возвращает длину LinkedList
+     * @noparams
+     * @return int
+     * **/
     @Override
     public int size() {
         return this.size;
-    }
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-    /**
-     * @function add добавляет объект в LinkedList
-     * @param item объект для добавления
-     * @return void
-     * **/
-    @Override
-    public void add(Object item) {
-        Node<E> node = new Node<>((E) item);
-        if (size == 0) {
-            this.head = node;
-        } else {
-            node.previous = this.tail;
-            this.tail.next = node;
+        @@ -46,10 +50,32 @@ public void add(Object item) {
+            this.tail = node;
+            size++;
         }
-        this.tail = node;
-        size++;
-    }
-    @Override
-    public void add(Object item, int index) {
-    }
-    @Override
-    public boolean remove(Object item) {
-        return false;
-    }
-    /**
-     * @function remove удаляет объект из LinkedList
-     * @param index индекст элемента для удаления
-     * @return Object
-     * **/
-    @Override
-    public Object remove(int index) {
-        checkIndex(index);
-        Node node = findIndexedNode(index);
-        if (node.previous == null) {
-            this.head = node.next;
-            this.head.previous = null;
-        }
-        else if (node.next == null) {
-            this.tail = node.previous;
-            this.tail.next = null;
-        }
-        else {
-            node.previous.next = node.next;
-            node.next.previous = node.previous;
-        }
-        this.size--;
-        return node.val;
-    }
 
-    @Override
-    public void clear() {
+        /**
+         * @function add добавляет объект на определенный индекс
+         * @param item объект для добавления
+         * @param index индекс куда нужно добавить item
+         * @return void
+         * **/
+        @Override
+        public void add(Object item, int index) {
 
-        this.head = null;
-        this.tail = null;
-        size = 0;
-    }
-    /**
-     * @function get возвращает объект из LinkedList под его индексом
-     * @param index индекс объекта
-     * @return E
-     * **/
-    @Override
-    public Object get(int index) {
-        checkIndex(index);
-        return findIndexedNode(index).val;
-    }
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-    @Override
-    public void sort() {
-    }
-    private Node findIndexedNode(int index) {
-        checkIndex(index);
-        Node toSearch;
-        toSearch = this.head;
-        while (index != 0) {
-            toSearch = toSearch.next;
-            index--;
+            Node oldNode = getNodeByIndex(index);
+            Node node = new Node<E>((E) item);
+            if (this.head.equals(oldNode)) {
+                node.next = this.head;
+                this.head.previous = node;
+                this.head = node;
+            } else if (this.tail.equals(oldNode)){
+                node.next = this.tail;
+                node.previous = this.tail.previous;
+                this.tail.previous.next = node;
+                this.tail.previous = node;
+            } else {
+                node.previous = oldNode.previous;
+                node.next = oldNode;
+                oldNode.previous.next = node;
+                oldNode.previous = node;
+            }
+            size++;
         }
-        return toSearch;
-    }
-    private void checkIndex(int index){
-        if(index < 0 || index>=size){
-            throw new IndexOutOfBoundsException();
-        }
-    }
-}
+
+        @Override
+        @@ -64,7 +90,7 @@ public boolean remove(Object item) {
+            @Override
+            public Object remove(int index) {
+                checkIndex(index);
+                Node node = findIndexedNode(index);
+                Node node = getNodeByIndex(index);
+
+                if (node.previous == null) {
+                    this.head = node.next;
+                    @@ -102,7 +128,7 @@ public void clear() {
+                        @Override
+                        public Object get(int index) {
+                            checkIndex(index);
+                            return findIndexedNode(index).val;
+                            return getNodeByIndex(index).val;
+                        }
+
+                        @Override
+                        @@ -119,7 +145,7 @@ public int lastIndexOf(Object o) {
+                            public void sort() {
+
+                            }
+                            private Node findIndexedNode(int index) {
+                                private Node getNodeByIndex(int index) {
+                                    checkIndex(index);
+                                    Node toSearch;
+                                    toSearch = this.head;
+                                    while (index != 0) {
+                                        toSearch = toSearch.next;
+                                        index--;
+                                    }
+                                    return toSearch;
+                                }
+                                private void checkIndex(int index){
+                                    if(index < 0 || index>=size){
+                                        throw new IndexOutOfBoundsException();
+                                    }
+                                }
+                            }
