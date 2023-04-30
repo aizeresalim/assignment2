c261fea127ebd0b72343f586061348991587bf66
aizeresalim <0424aizeka@gmail.com>
2023-05-01 02:29:11 +0600

    MyArrayListStack added

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,4 +1,14 @@
 import java.util.EmptyStackException;
 
+import java.util.EmptyStackException;
+
 public class MyArrayListStack<T> {
-    M
+    MyArrayList myArrayList = new <T>MyArrayList();
+
+    MyArrayListStack() {}
+
+    public void push(T element) {
+        myArrayList.add(element, 0);
+    }
+
+
