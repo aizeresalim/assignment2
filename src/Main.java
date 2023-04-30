122d6363268132aadede91bc0c0e7426cb5cc5d9
aizeresalim <0424aizeka@gmail.com>
2023-05-01 01:31:57 +0600

    my linked list queue tested in main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        //      ## MyLinkedListStack
+        System.out.println("MyLinkedListStack");
         MyLinkedListStack myLinkedListStack = new <Integer>MyLinkedListStack();
         myLinkedListStack.push(1);
         myLinkedListStack.push(2);
         myLinkedListStack.push(3);
         System.out.println(myLinkedListStack.pop());
         System.out.println(myLinkedListStack.pop());
-        System.out.println(myLinkedListStack.pop());
         System.out.println(myLinkedListStack.peek());
-        System.out.println(myLinkedListStack.peek());
-        System.out.println(myLinkedListStack.peek());
+
+        //      ## MyLinkedListQueue
+        System.out.println("MyLinkedListQueue");
+        MyLinkedListQueue myLinkedListQueue = new <Integer>MyLinkedListQueue();
+        myLinkedListQueue.enqueue(1);
+        myLinkedListQueue.enqueue(2);
+        myLinkedListQueue.enqueue(3);
+        System.out.println(myLinkedListQueue.dequeue());
+        System.out.println(myLinkedListQueue.dequeue());
+        System.out.println(myLinkedListQueue.peek());
+
+
+//      ## MyLinkedListQueue
+        System.out.println("MyArrayListStack");
+        MyLinkedListQueue myLinkedListQueue = new <Integer>MyLinkedListQueue();
+        myLinkedListQueue.enqueue(1);
+        myLinkedListQueue.enqueue(2);
+        myLinkedListQueue.enqueue(3);
+        System.out.println(myLinkedListQueue.dequeue());
+        System.out.println(myLinkedListQueue.dequeue());
+        System.out.println(myLinkedListQueue.peek());
     }
 }
 
