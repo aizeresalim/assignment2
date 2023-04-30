7cd4e65e808827308f7c03e246de4ada67376e31
aizeresalim <0424aizeka@gmail.com>
2023-05-01 00:53:32 +0600

    pop and peek

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList myArrayList = new MyArrayList<Integer>();
-        Integer[] e1 = new Integer[]{1,2,3};
-        Integer[] e2 = new Integer[]{4,5,6};
-        Integer[] e3 = new Integer[]{7,8,9};
-        myArrayList.addAllByIndex(e1, 0);
-        myArrayList.addAllByIndex(e2, 1);
-        myArrayList.addAllByIndex(e3, 2);
-        System.out.println(myArrayList);
         MyLinkedListStack myLinkedListStack = new <Integer>MyLinkedListStack();
         myLinkedListStack.push(1);
         myLinkedListStack.push(2);
         myLinkedListStack.push(3);
+        System.out.println(myLinkedListStack.pop());
+        System.out.println(myLinkedListStack.pop());
     }
 }
+
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,10 +1,22 @@
+import jdk.nio.mapmode.ExtendedMapMode;
+
 import java.util.EmptyStackException;
 
 public class MyLinkedListStack<T> {
-
     MyLinkedList myLinkedList = new <T>MyLinkedList();
     MyLinkedListStack() {}
     public void push(T element){
         myLinkedList.add(element, 0);
+    }
+    public T pop() {
+        isEmpty();
+        return (T) myLinkedList.remove(0);
+    }
+    public T peek() {
+        isEmpty();
+        return (T) myLinkedList.get(0);
+    }
+    private void isEmpty() {
+        if (myLinkedList.size() == 0) throw new EmptyStackException();
     }
 }
