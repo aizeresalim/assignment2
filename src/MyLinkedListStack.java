eeb36ed2e209742715cdeb11c642371b83bf71e1
aizeresalim <0424aizeka@gmail.com>
2023-05-01 01:16:22 +0600

    is empty

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,22 +1,38 @@
 import jdk.nio.mapmode.ExtendedMapMode;
-
 import java.util.EmptyStackException;
-
 public class MyLinkedListStack<T> {
+
     MyLinkedList myLinkedList = new <T>MyLinkedList();
-    MyLinkedListStack() {}
-    public void push(T element){
-        myLinkedList.add(element, 0);
-    }
-    public T pop() {
-        isEmpty();
-        return (T) myLinkedList.remove(0);
-    }
-    public T peek() {
-        isEmpty();
-        return (T) myLinkedList.get(0);
-    }
-    private void isEmpty() {
-        if (myLinkedList.size() == 0) throw new EmptyStackException();
-    }
-}
+
+
+        MyLinkedListStack() {
+        }
+
+        public void push(T element) {
+            myLinkedList.add(element, 0);
+        }
+
+        public T pop() {
+
+            isEmptyThrowException();
+            return (T) myLinkedList.remove(0);
+        }
+
+        public T peek() {
+        
+            isEmptyThrowException();
+            return (T) myLinkedList.get(0);
+        }
+        private void isEmpty() {
+            if (myLinkedList.size() == 0) throw new EmptyStackException();
+
+            private void isEmptyThrowException() {
+                if (isEmpty()) {
+                    throw new EmptyStackException();
+                }
+            }
+
+            public boolean isEmpty() {
+                return myLinkedList.size() == 0;
+            }
+        }
